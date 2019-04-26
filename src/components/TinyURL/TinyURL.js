2573429f6fb42c63c5011c690f42304e7e7e2bb5
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 18:23:27 -0500

    add originalURL to local state and input value in TinyURL.js, change input type to url

--- src/components/TinyURL/TinyURL.js
+++ src/components/TinyURL/TinyURL.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 
 class TinyURL extends Component {
+    state = {
+        originalURL: "",
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>URLs</h1>
-        <form>
-            <input></input>
-            <button>Create URL</button>
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>URLs</h1>
+                <form>
+                    <input type="url" value={this.state.originalURL}></input>
+                    <button>Create URL</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default TinyURL;
