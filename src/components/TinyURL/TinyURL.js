cb8c4d2c7ac168ed0b0f13a35e1409cf660b97bf
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 18:33:00 -0500

    create handleInputChange function and add it to onChange for input in TinyURL.js

--- src/components/TinyURL/TinyURL.js
+++ src/components/TinyURL/TinyURL.js
@@ -5,12 +5,18 @@ class TinyURL extends Component {
         originalURL: "",
     }
 
+    handleInputChange = propertyName => event => {
+        this.setState({
+          [propertyName]: event.target.value
+        });
+      };
+
     render() {
         return (
             <div>
                 <h1>URLs</h1>
                 <form>
-                    <input type="url" value={this.state.originalURL}></input>
+                    <input type="url" value={this.state.originalURL} onChange={this.handleInputChange('originalURL')}></input>
                     <button>Create URL</button>
                 </form>
             </div>
