94ad1421ad0853350cafb7554488dd8588019f81
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 14:43:52 -0500

    convert App.js file into a class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
