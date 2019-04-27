80ad5c9cd3ea67ad85e8d7852e3205df193d74c4
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 20:11:02 -0500

    change comment in validateURL function

--- src/components/TinyURL/TinyURL.js
+++ src/components/TinyURL/TinyURL.js
@@ -15,11 +15,10 @@ class TinyURL extends Component {
     handleFormSubmit = (event) => {
         event.preventDefault();
         this.validateURL(this.state.originalURL)
-        // console.log("URL submitted!");
     }
 
     validateURL = (userInput) => {
-        // use .test and regex to check if http or https is within the string
+        // use .test and regex to validate that the string is a URL
         let longURL = userInput;
         let regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
         if(regexp.test(longURL)){
@@ -32,7 +31,6 @@ class TinyURL extends Component {
                 showErrorMessage: true
             })
             console.log("not matches");
-
         }
     }
 
