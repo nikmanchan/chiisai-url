26c66c91fb12f55beb6ca095f8d1624a61b28a01
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 21:43:59 -0500

    import passport from user.strategy file into server.js file

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const sessionMiddleware = require('./modules/session-middleware');
 
+const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
