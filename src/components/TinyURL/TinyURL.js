9647a2d4e217b890011ff842aeaef377d1ed0844
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 19:49:11 -0500

    create validateURL function, add to handleFormSubmit function and add showErrorMessage to state

--- src/components/TinyURL/TinyURL.js
+++ src/components/TinyURL/TinyURL.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class TinyURL extends Component {
     state = {
         originalURL: "",
+        showErrorMessage: false,
     }
 
     handleInputChange = propertyName => event => {
@@ -13,8 +14,26 @@ class TinyURL extends Component {
 
     handleFormSubmit = (event) => {
         event.preventDefault();
-        console.log("URL submitted!");
-        
+        this.validateURL(this.state.originalURL)
+        // console.log("URL submitted!");
+    }
+
+    validateURL = (userInput) => {
+        // use .test and regex to check if http or https is within the string
+        let oldURL = userInput;
+        let regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if(regexp.test(oldURL)){
+            this.setState({
+                showErrorMessage: false
+            })
+            console.log("matches!");
+        }else {
+            this.setState({
+                showErrorMessage: true
+            })
+            console.log("not matches");
+
+        }
     }
 
     render() {
@@ -22,7 +41,7 @@ class TinyURL extends Component {
             <div>
                 <h1>URLs</h1>
                 <form onSubmit={this.handleFormSubmit}>
-                    <input type="url" required value={this.state.originalURL} onChange={this.handleInputChange('originalURL')}></input>
+                    <input required value={this.state.originalURL} onChange={this.handleInputChange('originalURL')}></input>
                     <button value="submit" type="submit">Create URL</button>
                 </form>
                 <table>
