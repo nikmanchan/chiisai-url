9518c3f51d4fb09d4854c8851ba000b58cad30f8
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 21:33:44 -0500

    setup serializeUser function for passport in user.strategy.js

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -3,4 +3,8 @@ const LocalStrategy = require('passport-local').Strategy;
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
 module.exports = passport;
