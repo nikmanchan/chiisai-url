e6b899db7baf31f083dfa535510f6ea409ede136
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 21:06:10 -0500

    setup userRouter file in server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const app = express();
 const bodyParser = require('body-parser');
 
+// Route includes
+const userRouter = require('./routes/user.router');
+
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
