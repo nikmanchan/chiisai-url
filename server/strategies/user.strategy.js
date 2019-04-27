fed31fcbf977e5b093e3dd5199b165d2e9fcb33f
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 21:34:45 -0500

    setup deserializeUser function for passport in user.strategy.js file to allow select statement for user

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -5,6 +5,24 @@ const pool = require('../modules/pool');
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
-  });
+});
+
+passport.deserializeUser((id, done) => {
+    pool.query('SELECT id, username FROM users WHERE id = $1', [id]).then((result) => {
+        // Handle Errors
+        const user = result && result.rows && result.rows[0];
+
+        if (!user) {
+            // user not found
+            done(null, false, { message: 'Incorrect credentials.' });
+        } else {
+            // user found
+            done(null, user);
+        }
+    }).catch((err) => {
+        console.log('query err ', err);
+        done(err);
+    });
+});
 
 module.exports = passport;
