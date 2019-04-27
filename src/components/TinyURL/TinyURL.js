11f26ad6897a0d896b3f0c6f358d25142b155bb5
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 20:07:42 -0500

    rename oldURL variable to longURL in validateURL function

--- src/components/TinyURL/TinyURL.js
+++ src/components/TinyURL/TinyURL.js
@@ -20,9 +20,9 @@ class TinyURL extends Component {
 
     validateURL = (userInput) => {
         // use .test and regex to check if http or https is within the string
-        let oldURL = userInput;
+        let longURL = userInput;
         let regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-        if(regexp.test(oldURL)){
+        if(regexp.test(longURL)){
             this.setState({
                 showErrorMessage: false
             })
