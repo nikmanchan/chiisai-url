3e7f06ee0ffd7097b2c5ee3512f5ffcace767843
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 21:41:03 -0500

    setup logout post route in user.router.js with passport built in logout method

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -30,4 +30,11 @@ router.post('/login', userStrategy.authenticate('local'), (req, res) => {
     res.sendStatus(200);
 });
 
+// clear all server session information about this user
+router.post('/logout', (req, res) => {
+    // Use passport's built-in method to log out the user
+    req.logout();
+    res.sendStatus(200);
+});
+
 module.exports = router;
