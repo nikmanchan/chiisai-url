9ada82cad3abfecb5d2a05403ca99c8127eda81d
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 21:50:08 -0500

    add user from userReducer to index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import urlData from './urlReducer'
+import user from './userReducer'
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -9,6 +10,7 @@ import urlData from './urlReducer'
 // This is what we get when we use 'state' inside of 'mapStateToProps'
 const rootReducer = combineReducers({
     urlData, // will store URL data in redux state 
+    user, // will have an id and username if someone is logged in
 });
 
 export default rootReducer;
