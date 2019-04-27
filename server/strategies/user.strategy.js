51de2c83c39320e7fe1cda3bb34cfc4956752d8e
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-26 21:36:39 -0500

    create login functionality with select statement from users table by the username column

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -25,4 +25,28 @@ passport.deserializeUser((id, done) => {
     });
 });
 
+// Does actual work of logging in
+passport.use('local', new LocalStrategy({
+    passReqToCallback: true,
+    usernameField: 'username',
+}, ((req, username, password, done) => {
+    pool.query('SELECT * FROM users WHERE username = $1', [username])
+        .then((result) => {
+            const user = result && result.rows && result.rows[0];
+            if (user && encryptLib.comparePassword(password, user.password)) {
+                // all good! Passwords match!
+                done(null, user);
+            } else if (user) {
+                // not good! Passwords don't match!
+                done(null, false, { message: 'Incorrect credentials.' });
+            } else {
+                // not good! No user with that name
+                done(null, false);
+            }
+        }).catch((err) => {
+            console.log('error', err);
+            done(null, {});
+        });
+})));
+
 module.exports = passport;
