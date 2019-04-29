b0f2f60197ca648dd53c4f49a966eeb566860aaa
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-29 13:37:51 -0500

    import SignUp and Login components into App.js and create routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,8 @@ import {
   Switch
 } from "react-router-dom";
 import TinyURL from "../TinyURL/TinyURL"
+import SignUp from "../SignUp/SignUp";
+import Login from "../Login/Login";
 
 class App extends Component {
 
@@ -18,6 +20,8 @@ class App extends Component {
           <div>
             <Switch>
               <Route path="/" exact component={TinyURL}/>
+              <Route path="/sign-up" exact component={SignUp}/>
+              <Route path="/login" exact component={Login}/>
               {/* If none of the other routes matched, we will show a 404. */}
               <Route render={() => <h1>404</h1>} />
             </Switch>
