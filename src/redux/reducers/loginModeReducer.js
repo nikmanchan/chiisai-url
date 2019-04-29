5f03ce31f10e31756c205817d92f6fc37fd4c910
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-29 09:47:37 -0500

    create loginModeReducer file and configure to toggle register or login

---
+++ src/redux/reducers/loginModeReducer.js
@@ -0,0 +1,14 @@
+const loginMode = (state = 'register', action) => {
+    switch (action.type) {
+      case 'SET_TO_LOGIN_MODE':
+        return 'login';
+      case 'SET_TO_REGISTER_MODE':
+        return 'register';
+      default:
+        return state;
+    }
+  };
+
+// loginMode will be on the redux state at:
+// state.loginMode
+  export default loginMode;
