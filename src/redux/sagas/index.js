37d10d2b128bdc404ab9d6ed9e82f2253f260897
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-29 10:32:25 -0500

    add user and registration sagas to index.js file

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,5 +1,7 @@
 import { all } from 'redux-saga/effects';
 import urlSaga from './urlSaga'
+import userSaga from './userSaga'
+import registrationSaga from './registrationSaga'
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -11,5 +13,7 @@ import urlSaga from './urlSaga'
 export default function* rootSaga() {
   yield all([
     urlSaga(),
+    userSaga(),
+    registrationSaga(),
   ]);
 }
