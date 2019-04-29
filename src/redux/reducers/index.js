9ee1dc8a07dd3508409085ce87e59d34a55aa640
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-29 09:49:03 -0500

    add loginMode to rootReducer in index.js file

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import urlData from './urlReducer'
 import user from './userReducer'
+import loginMode from './loginModeReducer';
+
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -11,6 +13,7 @@ import user from './userReducer'
 const rootReducer = combineReducers({
     urlData, // will store URL data in redux state 
     user, // will have an id and username if someone is logged in
+    loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
 });
 
 export default rootReducer;
