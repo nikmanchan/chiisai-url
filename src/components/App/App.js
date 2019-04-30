65a6fa79f08421bbf1d00c8ea0c66a27b3d0ad94
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-30 14:22:18 -0500

    remove redirect from App.js file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,8 +21,6 @@ class App extends Component {
         <Router>
           <div>
             <Switch>
-              {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-              <Redirect exact from="/" to="/chiisai" />
               {/* For protected routes, the view could show one of several things on the same route.
                 Visiting localhost:3000/home will show the UserDashboard if the user is logged in.
                 If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
