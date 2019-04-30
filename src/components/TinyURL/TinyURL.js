b5948f03190dfb9e37e1dab0ad82edba678fcb3f
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-29 19:25:34 -0500

    map redux state to props for urlData and add Sign Out button to TinyURL component

--- src/components/TinyURL/TinyURL.js
+++ src/components/TinyURL/TinyURL.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 class TinyURL extends Component {
     state = {
@@ -20,17 +21,17 @@ class TinyURL extends Component {
     validateURL = (userInput) => {
         // use .test and regex to validate that the string is a URL
         let longURL = userInput;
-        let regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-        if(regexp.test(longURL)){
+        let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(longURL)) {
             this.setState({
                 showErrorMessage: false
             })
             console.log("matches!");
-        }else {
+        } else {
             this.setState({
                 showErrorMessage: true
             })
-            console.log("not matches");
+            console.log("not matches!");
         }
     }
 
@@ -38,6 +39,11 @@ class TinyURL extends Component {
         return (
             <div>
                 <h1>URLs</h1>
+                <button
+                    onClick={() => this.props.dispatch({ type: 'LOGOUT' })}
+                >
+                    Sign Out
+                </button>
                 <form onSubmit={this.handleFormSubmit}>
                     <input required value={this.state.originalURL} onChange={this.handleInputChange('originalURL')}></input>
                     {this.state.showErrorMessage === true && <p>Error: Long URL is not a valid URL</p>}
@@ -53,7 +59,7 @@ class TinyURL extends Component {
                     </thead>
                     <tbody>
                         {/* setup to map through standard data cells with url data stored in redux state */}
-                        
+
                     </tbody>
                 </table>
             </div>
@@ -61,4 +67,9 @@ class TinyURL extends Component {
     }
 }
 
-export default TinyURL;
+// Instead of taking everything from state, we just want the URL data.
+const mapStateToProps = state => ({
+    urlData: state.urlData
+});
+
+export default connect(mapStateToProps)(TinyURL);
