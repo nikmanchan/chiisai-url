1705ec2c47112b68ab64ca54cc745eeea225cc44
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-30 09:38:43 -0500

    change addURL function to generator function and set up POST route to /api/url with URL data in urlSaga file

--- src/redux/sagas/urlSaga.js
+++ src/redux/sagas/urlSaga.js
@@ -1,15 +1,20 @@
-import { takeLatest } from 'redux-saga/effects';
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
 
 // worker Saga: will be fired on 'SEND_URL_DATA' actions
-function addURL() {
-    console.log('adding URL data!');
-
+function* addURL(action) {
+    try {
+        // sends URL data to the server
+        yield axios.post('api/url', action.payload);
+    } catch (error) {
+        console.log('Error with URL data POST to server:', error);
+    }
 }
 
 
 
 function* urlSaga() {
-  yield takeLatest('SEND_URL_DATA', addURL);
+    yield takeLatest('SEND_URL_DATA', addURL);
 }
 
 export default urlSaga;
