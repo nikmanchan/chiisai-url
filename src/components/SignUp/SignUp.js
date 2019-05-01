da529fac96dd77c255eaf12e4e5aa8e4391a30cb
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-05-01 08:35:38 -0500

    create primaryUserButton class and apply to sign up button

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -77,6 +77,7 @@ class SignUp extends Component {
                         />
                         <br></br>
                         <button
+                            className="primaryUserButton"
                             type="submit"
                             name="submit"
                             value="Register"
