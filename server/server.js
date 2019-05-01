248d20fd679d07f180d649a03b6ef0f4e773f1be
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-30 20:57:27 -0500

    create chiisai.router file and setup redirect and hit_count update for shortened_URL and update routes in server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const urlRouter = require('./routes/url.router');
+const chiisaiRouter = require('./routes/chiisai.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -26,34 +27,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/url', urlRouter);
-app.use('/chiisai/:id', function (req, res, next) {
-    console.log("hit chiisai route!")
-    let id = req.params.id;
-
-    const queryText = `SELECT * FROM "shortened_URLs" WHERE id = $1;`;
-    // Get all urlData by id from shortened_URLs column
-    pool.query(queryText, [id])
-        .then(results => {
-            // set urlData
-            let urlData = results.rows[0].original_URL;
-            const queryText2 = `UPDATE "shortened_URLs" SET "hit_count" = "hit_count" + 1 WHERE "id" = $1;`;
-
-            // Update hit_count
-            pool.query(queryText2, [id])
-                .then(results => {
-                    console.log('success with update hit_count', results.rows)
-                })
-                .catch(error => {
-                    console.log('ERROR with updating hit count in database:', error);
-                    res.sendStatus(500);
-                })
-            res.redirect(urlData);
-        })
-        .catch(error => {
-            console.log('ERROR with GET URL data from database:', error);
-            res.sendStatus(500);
-        })
-});
+app.use('/chiisai', chiisaiRouter);
 
 // Serve static files
 app.use(express.static('build'));
