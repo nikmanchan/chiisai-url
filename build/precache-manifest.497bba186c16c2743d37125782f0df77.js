bec2be554a47bccaf594f902d98dbcb6595cf1c4
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-04-30 21:27:33 -0500

    create production build files

---
+++ build/precache-manifest.497bba186c16c2743d37125782f0df77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768b8ab75791e28839794d4ce5bb2891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e9aa766b810db94a8bd",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0bfdb7250d420d7d1ead",
+    "url": "/static/js/2.ab8564e8.chunk.js"
+  },
+  {
+    "revision": "7e9aa766b810db94a8bd",
+    "url": "/static/js/main.2227be14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
