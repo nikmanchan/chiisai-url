3166d16156e00754929f3e7784524c801604ba71
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-05-01 08:49:23 -0500

    add styling to loginForm id

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import "./Login.css";
 
 class LoginPage extends Component {
 
